50222c11c0d2aab414dec5461ae1a560f05e4fde
akito <anandaalieff@gmail.com>
2024-05-20 21:50:29 +0700

    ganti icon dan juga hapus navbar

--- src/tampilan/NavbarPage.js
+++ src/tampilan/NavbarPage.js
@@ -1,31 +1,17 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 
 const NavbarPage = () => {
   return (
-    <Navbar expand="lg">
-      <Container>
-        <Navbar.Brand href="/home">
-          <Nav.Link href="/home" style={{ color: "black", fontWeight: "bold" }}>
-            Home
-          </Nav.Link>
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto right">
-            <Nav.Link
-              href="/history"
-              style={{ color: "black", fontWeight: "bold" }}
-            >
-              History
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container fluid className="d-flex justify-content-between align-items-center py-2">
+      <Nav.Link href="/home" style={{ color: "black", fontWeight: "bold" }}>
+        Home
+      </Nav.Link>
+      <Nav.Link href="/history" style={{ color: "black", fontWeight: "bold" }}>
+        History
+      </Nav.Link>
+    </Container>
   );
 };
 
